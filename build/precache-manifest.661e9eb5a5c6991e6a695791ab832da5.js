f80655be9750d85bd51cae6ba3690b5b369bde28
StankoVP <Stanko.V.P@yandex.ru>
2024-01-09 21:55:54 +0300

    build not ignored

---
+++ build/precache-manifest.661e9eb5a5c6991e6a695791ab832da5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcf199087b40073c1b155e880ae65080",
+    "url": "/react-movies/index.html"
+  },
+  {
+    "revision": "c19d8d6f2825002021e0",
+    "url": "/react-movies/static/css/main.a48a5147.chunk.css"
+  },
+  {
+    "revision": "bc853196e31e2d32a6c9",
+    "url": "/react-movies/static/js/2.a2b181a3.chunk.js"
+  },
+  {
+    "revision": "c19d8d6f2825002021e0",
+    "url": "/react-movies/static/js/main.acdd2e33.chunk.js"
+  },
+  {
+    "revision": "cdb8474b72bd7dff1c63",
+    "url": "/react-movies/static/js/runtime~main.bf100f6d.js"
+  }
+]);
